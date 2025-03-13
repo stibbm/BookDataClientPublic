ff98e61b803a917e197f53e0da5f276d63e99405
Matthew Stibbins <matthewstibbins@gmail.com>
2025-03-13 06:54:45 -0700

    setup base java spring boot command line project

---
+++ src/main/java/matt/book/data/client/BookDataClientPublicApplication.java
@@ -0,0 +1,13 @@
+package matt.book.data.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDataClientPublicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDataClientPublicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/matt/book/data/client/BookDataClientPublicApplicationTests.java
@@ -0,0 +1,13 @@
+package matt.book.data.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDataClientPublicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
